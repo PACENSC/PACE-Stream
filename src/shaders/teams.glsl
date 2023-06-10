a8a7deccb311687eb4ad0ecde522ac33641a23e4
shenoying <85912755+shenoying@users.noreply.github.com>
2023-06-10 03:50:37 -0400

    added raymarching, team colors

--- src/shaders/teams.glsl
+++ src/shaders/teams.glsl
@@ -2,86 +2,201 @@
 precision highp float;
 #endif
 
-uniform float    u_time;
-uniform vec2     u_resolution;
-uniform vec2     u_mouse;
-uniform vec3     u_camera;
+uniform float     u_time;
+uniform vec2      u_resolution;
+uniform vec2      u_mouse;
+uniform vec3      u_camera;
 
 #define PI          3.141592653589
 #define TAU         2.0 * PI
 #define MAX_STEPS   100 // Mar Raymarching steps
 #define MAX_DIST    100. // Max Raymarching distance
 #define SURF_DIST   .01 // Surface Distance
- 
-float GetDist(vec3 p) 
+
+float opSmoothUnion(float d1, float d2, float k) 
 {
-  vec4 s = vec4(0, 1, 6, 1); //Sphere xyz is position w is radius
-  float sphereDist = length(p - s.xyz) - s.w;
-  float planeDist  = p.y;
-  float d = min(sphereDist,planeDist);
-  return d;
-}
- 
-float RayMarch(vec3 ro, vec3 rd) 
-{
-  float dO = 0.; //Distane Origin
-  for(int i = 0; i < MAX_STEPS; i++)
-  {
-    vec3 p = ro + rd * dO;
-    float ds = GetDist(p); // ds is Distance Scene
-    dO += ds;
-    if(dO > MAX_DIST || ds < SURF_DIST) 
-      break;
-  }
-  return dO;
-}
- 
-vec3 GetNormal(vec3 p)
-{ 
-    float d = GetDist(p); // Distance
-    vec2 e = vec2(.01, 0); // Epsilon
-    vec3 n = d - vec3(
-    GetDist(p - e.xyy),
-    GetDist(p - e.yxy),
-    GetDist(p - e.yyx));
- 
-    return normalize(n);
+	float h = clamp( 0.5 + 0.5*(d2-d1)/k, 0.0, 1.0 );
+	return mix( d2,  d1,  h ) - k*h*(1.0-h); 
 }
 
-float GetLight(vec3 p)
-{ 
-    // Directional light
-    vec3 lightPos = vec3(5. * sin(u_time), 5., 5.0 * cos(u_time)); // Light Position
-    vec3 l = normalize(lightPos - p); // Light Vector
-    vec3 n = GetNormal(p); // Normal Vector
-   
-    float dif = dot(n, l); // Diffuse light
-    dif = clamp(dif, 0., 1.); // Clamp so it doesnt go below 0
-   
-    // Shadows
-    float d = RayMarch(p + n * SURF_DIST * 2., l); 
-     
-    if(d < length(lightPos - p)) dif *= .1;
+float opSmoothSubtraction(float d1, float d2, float k) 
+{
+	float h = clamp( 0.5 - 0.5*(d2+d1)/k, 0.0, 1.0 );
+	return mix( d2, -d1, h ) + k*h*(1.0-h); }
+
+float opSmoothIntersection(float d1, float d2, float k) 
+{
+	float h = clamp( 0.5 - 0.5*(d2-d1)/k, 0.0, 1.0 );
+	return mix( d2, d1, h ) + k*h*(1.0-h); 
+}
+
+float sdSphere(vec3 p, float s)
+{
+	return length(p) - s;
+}
+
+float sdBox(vec3 p, vec3 b)
+{
+	vec3 q = abs(p) - b;
+  	return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+
+float sdRoundBox(vec3 p, vec3 b, float r)
+{
+  	vec3 q = abs(p) - b;
+  	return length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0) - r;
+}
+
+float sdCappedCylinder(vec3 p, float h, float r)
+{
+  	vec2 d = abs(vec2(length(p.xz), p.y)) - vec2(r, h);
+  	return min(max(d.x,d.y),0.0) + length(max(d,0.0));
+}
+
+float sdTorus(vec3 p, vec2 t)
+{
+  	vec2 q = vec2(length(p.xz) - t.x, p.y);
+  	return length(q) - t.y;
+}
+
+float sdVerticalCapsule(vec3 p, float h, float r)
+{
+  	p.y -= clamp(p.y, 0.0, h);
+	return length(p) - r;
+}
+
+float drawBuzzerButton(vec3 p, out vec3 col)
+{
+  	float buzzerRing = sdTorus(p - vec3(0, 0.0, 0), vec2(0.63, 0.10));
+  	float buttonDivet = sdSphere(p - vec3(0., 2.5, 0.), 0.87);
+	float buzzerButton = sdCappedCylinder(p - vec3(0., 1.5, 0.0), .250, 0.53);	
+	// buzz function : + 0.1 * abs(sin(u_time * TAU))
+	float d = buzzerButton;
+	d = min(d, buzzerButton);
+	d = max(d, -buttonDivet);
+
+	col =vec3(0.8, 0.1, 0.1) ;
+
+	return d;
+}
+
+float drawBuzzer(vec3 p, bool isRedLed, out vec3 col)
+{
+  	float buzzerBox =  sdRoundBox(p / vec3(3.118, 1.13, 1.9818), vec3(1, 1, 1), 0.30);
+  	float ledHole = sdCappedCylinder(p - vec3(0., 1.5, 1.2), .20, 0.25);
+  	float led = sdVerticalCapsule(p - vec3(0., 1.2, 1.2), 0.28, 0.21);
+	float buzzerHole = sdCappedCylinder(p - vec3(0., 1.5, 0.0), .67, 0.67);
+
+	float d = buzzerBox;
+	d = max(d, -buzzerHole);
+  	d = max(d, -ledHole);
+
+	float boxD = d;
+
+  	d = min(d, led);
+
+	vec3 buttonCol;
+	float buttonD = min(d, drawBuzzerButton(p + vec3(0., 0.2 * abs(sin(u_time * TAU)), 0.), buttonCol));
+	
+	d = min(boxD, buttonD);
+	d = min(d, led);
+
+	float colD = boxD;
+	vec3 finalCol = vec3(0.15);
+	if (buttonD < colD) {
+		colD = buttonD;
+		finalCol = buttonCol;
+	}
+	if (led <= colD) {
+		colD = led;
+		float dominant = 0.75 * abs(sin(u_time * PI * 1.5)) + 0.20;
+		finalCol = (isRedLed) ? vec3(dominant, 0.10, 0.15) : vec3(0.15, dominant, .1);
+	}
+	col = finalCol;
+
+  	return d;
+}
+
+float getDist(vec3 p, out vec3 col) 
+{
+	vec4 s = vec4(0, 1, 6, 1); //Sphere xyz is position w is radius
+	float sphereDist = length(p - s.xyz) - s.w;
+	float planeDist  = p.y;
+	float buzzerBox = sdRoundBox(p, vec3(2, 3.2, 1.6), 2.15);
+	vec3 buzzerCol = vec3(0);
+	float d = drawBuzzer(p - vec3(0, -5.8, 19), false, buzzerCol); //min(buzzerBox, planeDist);
+	//d = min(d, buzzerBox);
+	col = buzzerCol;
+	return d;
+}
  
-    return dif;
+float rayMarch(vec3 ro, vec3 rd, out vec3 col) 
+{
+	float dO = 0.; //Distane Origin
+	vec3 outCol;
+	for(int i = 0; i < MAX_STEPS; i++)
+	{
+		vec3 p = ro + rd * dO;
+		vec3 iterCol;
+		float ds = getDist(p, outCol); // ds is Distance Scene
+		dO += ds;
+		if(dO > MAX_DIST || ds < SURF_DIST) 
+		break;
+	}
+	col = outCol;
+	return dO;
+}
+ 
+vec3 getNormal(vec3 p)
+{ 
+	vec3 _ = vec3(0);
+	float d = getDist(p, _); // Distance
+	vec2 e = vec2(.01, 0); // Epsilon
+	vec3 n = d - vec3(
+	getDist(p - e.xyy, _),
+	getDist(p - e.yxy, _),
+	getDist(p - e.yyx, _));
+ 
+	return normalize(n);
+}
+
+float getLight(vec3 p)
+{ 
+	// Directional light
+	vec3 lightPos = vec3(5.0, 5.0, 5.0); // Light Position
+	vec3 l = normalize(lightPos - p); // Light Vector
+	vec3 n = getNormal(p); // Normal Vector
+   
+	float dif = dot(n, l); // Diffuse light
+	dif = clamp(dif, 0., 1.); // Clamp so it doesnt go below 0
+   
+	// Shadows
+	vec3 _ = vec3(0);
+	float d = rayMarch(p + n * SURF_DIST * 2., l, _); 
+	 
+	if(d < length(lightPos - p)) dif *= .1;
+ 
+	return dif;
 }
  
 void main()
 {
-    vec2 uv = (gl_FragCoord.xy - .5 * u_resolution.xy) / u_resolution.y;
-     
-    vec3 ro = vec3(0, 1, 0); // Ray Origin/Camera
-    vec3 rd = normalize(vec3(uv.x, uv.y, 1)); // Ray Direction
+	vec2 uv = (gl_FragCoord.xy - .5 * u_resolution.xy) / u_resolution.y;
+	 
+	vec3 ro = vec3(0, 1, 0); // Ray Origin/Camera
+	vec3 rd = normalize(vec3(uv.x, uv.y, 1)); // Ray Direction
    
-    float d = RayMarch(ro, rd); // Distance
+	vec3 surfCol = vec3(0);
+	float d = rayMarch(ro, rd, surfCol); // Distance
    
-    vec3 p = ro + rd * d;
-    float dif = GetLight(p); // Diffuse lighting
-    d *= .2;
-    vec3 color = vec3(dif);
-    //color += GetNormal(p);
-    //float color = GetLight(p);
+	vec3 p = ro + rd * d;
+	float dif = getLight(p); // Diffuse lighting
+	d *= .2;
+	vec3 color = vec3(dif) * surfCol;
+	// color = surfCol;
+	//color += getNormal(p);
+	//float color = getLight(p);
  
-    // Set the output color
-    gl_FragColor = vec4(color,1.0);
+	// Set the output color
+	gl_FragColor = vec4(color,1.0);
 }
--- src/ts/game_info.ts
+++ src/ts/game_info.ts
@@ -10,38 +10,40 @@ let cols:number[] = [0,1,2];
 // designed to be consistent across all teams
 // from a given school
 let TeamCols = new Map ([
-    ["auburn", [0xD23217, 0x585858, 0x1337D3, 0xC4C4C4]], ["barrington",  [0xE34553, 0x272727, 0xC2C2C2, 0x6AE11E]],  
-    ["belmont", [0]], ["belvidere", [0]],
-    ["canyon_crest", [0]], ["carbondale", [0]],
-    ["carl_sandburg", [0]], ["chattahoochee", [0]],
-    ["choate", [0]], ["cincinnati", [0]],
-    ["dallas", [0]], ["dcc", [0]],
-    ["dcd", [0]], ["elkhorn_north", [0]],
-    ["fremd", [0]], ["heights", [0]],
-    ["hinsdale_central", [0]], ["hoover", [0]],
-    ["hunter", [0]], ["innovation", [0]],
-    ["irvington", [0]], ["john_adams", [0]],
-    ["johns_creek", [0]], ["kinkaid", [0]],
-    ["lincoln-way", [0]], ["maggie_walker", [0]],
-    ["mercer", [0]], ["mexico", [0]],
-    ["midtown", [0]], ["mira_loma", [0]],
-    ["moberly", [0]], ["new_brighton", [0]],
-    ["norman", [0]], ["norris", [0]],
-    ["northmont", [0]], ["parkway_west", [0]],
-    ["pl_dunbar", [0]], ["plymouth", [0]],
-    ["rm", [0]], ["saint_joseph", [0]],
-    ["solon", [0]], ["st_marks", [0]],
-    ["stevenson", [0]], ["tj", [0]],
-    ["troy", [0]], ["uni_lab", [0]],
-    ["usn", [0]], ["walter_payton", [0]],
-    ["washington", [0]], ["waukee", [0]],
-    ["west_point", [0]], ["winnebago", [0]],
-    ["w_churchill", [0]], ["woodland", [0]]
+    ["auburn",      [0xD23217, 0x585858, 0x1337D3, 0xC4C4C4]],  ["barrington",      [0xE34553, 0x272727, 0xC2C2C2, 0x6AE11E]],  
+    ["belmont",     [0xD43D2B, 0x260CC7, 0x1120B3, 0x7A25DA]],  ["belvidere",       [0x9C549C, 0xD7D934, 0x3177CE, 0xC7C7C7]],
+    ["canyon",      [0xC61522, 0x363834, 0xC3C234, 0xD98A26]],  ["carbondale",      [0x343434, 0xD4D4D4, 0x2675D9, 0xD926CE]],
+    ["c-sandburg",  [0x230DD8, 0xDADA43, 0x4A52B5, 0xBEAD24]],  ["chattahoochee",   [0x320CC7, 0xCBCB54, 0x5A61C3, 0xEAC715]],
+    ["choate",      [0xEBEB23, 0x2323A2, 0x4A57C2, 0xA4B4C4]],  ["cincinnati",      [0x8A55AA, 0x113563, 0xEAEAEA, 0xA5B5CA]],
+    ["dallas",      [0x7800DB, 0xE9E8E7, 0xABA9AB, 0x162901]],  ["dcc",             [0xEDEDED, 0x2323C3, 0x5E66A1, 0x4245BD]],
+    ["dcd",         [0x1212D2, 0xD3D322, 0xEE11BA, 0x1AE530]],  ["elkhorn",         [0x2E2E2E, 0xACACAC, 0xEE2411, 0x1AD2E5]],
+    ["fremd",       [0xEAF30D, 0x029715, 0xE01FDE, 0x1A4CE5]],  ["heights",         [0x801010, 0xE3E3E3, 0xCBCB26, 0xB4854B]],
+    ["hinsdale",    [0xE0211F, 0xE3E3E3, 0x26CBA0, 0x4B96B4]],  ["hoover",          [0xE0791F, 0x1E1E1E, 0xF1FCFA, 0x59B44B]],
+    ["hunter",      [0x931FE0, 0xF1FCFC, 0xEAC3EE, 0x11230E]],  ["innovation",      [0xE0691F, 0xCA4444, 0xF1FCFC, 0x757975]],
+    ["irvington",   [0xE7E7E7, 0x1E00DD, 0x6864E2, 0x51519D]],  ["john-adams",      [0xFF2626, 0xF6F6F6, 0x4C47FF, 0xA945A7]],
+    ["johns-creek", [0xFF2626, 0xF6F6F6, 0xFFD947, 0xC82525]],  ["kinkaid",         [0xA926FF, 0xF6F6F6, 0xFFD514, 0xA34A5E]],
+    ["lincoln-way", [0x2673FF, 0x1D1D1D, 0x14FFC2, 0x30A8BE]],  ["maggie-walker",   [0x14D609, 0xEAEAED, 0xF0EA5B, 0x14B1DA]],
+    ["mercer",      [0xF67322, 0x2D2C34, 0xF4F3E9, 0x329FBC]],  ["mexico",          [0xF72C20, 0xEEEDF0, 0xB3B3B3, 0x15B1D9]],
+    ["midtown",     [0xBBB5B5, 0xD40000, 0x6885FF, 0x0CE141]],  ["mira-loma",       [0x99CCFF, 0xCC0000, 0xFEFEFE, 0x8D0CE1]],
+    ["moberly",     [0xC7161C, 0x2A52BE, 0xFEFEFE, 0xB42BC3]],  ["new-brighton",    [0xB9141A, 0xDDB70B, 0x5AEB00, 0x61DCB2]],
+    ["norman",      [0x141414, 0xDDB70B, 0x00EB4D, 0x4E4EEF]],  ["norris",          [0xF0F0F0, 0xDD390B, 0x007BEB, 0x83EF4E]],
+    ["northmont",   [0x1BDE2B, 0xF4F22C, 0xCE1DCC, 0x4E6DEF]],  ["parkway-west",    [0x041105, 0xFDFCD7, 0xE00B0B, 0x4EAAEF]],
+    ["pl-dunbar",   [0x0A0B0A, 0xF31616, 0x0BE02E, 0x4E75EF]],  ["plymouth",        [0x4055FF, 0xF4F3F3, 0x0BE02E, 0xEF524E]],
+    ["r-m",         [0x121214, 0xF7E600, 0xD10BE0, 0x4EBCEF]],  ["saint-joseph",    [0x35DAF7, 0xF0F0EF, 0x221F22, 0xEF5B4E]],
+    ["solon",       [0xF1F5F5, 0xF6E724, 0x3F4CBB, 0xD76565]],  ["saint-marks",     [0x3A2EFE, 0xFBEE3D, 0xEFEFF1, 0xDD7CA1]],
+    ["stevenson",   [0x0CCC0C, 0xF3E95C, 0xEFEFF1, 0x6AA0EF]],  ["tjhsst",          [0xEDF3ED, 0xFB5954, 0x3528C8, 0x5EF056]],
+    ["troy",        [0x111611, 0x949090, 0xDC1414, 0x56F0D0]],  ["uni-lab",         [0xFFA306, 0x3247F2, 0xDC14CE, 0x56F05A]],
+    ["usn",         [0xCF0600, 0xA03636, 0xE03DD4, 0xB95C1B]],  ["west-point",      [0x1520DB, 0xF99600, 0x161616, 0xECE30B]],
+    ["washington",  [0x1F2BF4, 0xF4F4F2, 0xF91313, 0x1BF551]],  ["walter-payton",   [0x9105ED, 0xFCDE0E, 0xF94513, 0xF51BC3]],
+    ["west-point",  [0xE7E6E6, 0x332D05, 0xCF3105, 0x888888]],  ["winnebago",       [0xFDAF18, 0x332D05, 0xBA0DC7, 0x28E997]],
+    ["w-churchill", [0x1E2EE1, 0xE1291E, 0x1EE1DE, 0xEDF0EB]],  ["woodlands",       [0x090909, 0xF6B909, 0xC410E4, 0x39FAC3]]
 ]);
 
-// the names of the two teams playing a game
-let team_one = "Barrington";
-let team_two = "Auburn";
+// the names of the two teams playing a game,
+// according to the **lowercase** keys written
+// in the table above
+let team_one = "barrington";
+let team_two = "r-m";
 
 /**
  * Function to convert hex to RGB
@@ -61,60 +63,3 @@ function linkUniforms()
 {
 
 }
-
-/**
-    // Auburn High School A (Rockford, IL) – Fully Paid
-    // Barrington A (Barrington, IL) – Fully Paid
-    // Belmont (Belmont, MA) – Fully Paid
-    // Belvidere High School (Belvidere, IL) – Fully Paid
-    // Canyon Crest (San Diego, CA)
-    // Carbondale High School (Carbondale, IL) – Fully Paid
-    // Carl Sandburg High School A (Plainfield, IL) – Fully Paid
-    // Chattahoochee A (Johns Creek, GA) – Fully Paid
-    // Choate A (Wallingford, CT) – Fully Paid
-    // Cincinnati Hills Christian Academy A (Cincinnati, OH) – Fully Paid
-    // Dallas County Academic Team (Fair Grove, MO) – Fully Paid
-    // Detroit Catholic Central A (Novi, MI) – Fully Paid
-    // Detroit Country Day School (Beverly Hills, MI) – Fully Paid
-    // Elkhorn North High School (Omaha, NE) – Fully Paid
-    // Fremd A (Palatine, IL) – Fully Paid
-    // Heights High School (Houston, TX) – Fully Paid
-    // Hinsdale Central (Hinsdale, IL) – Fully Paid
-    // Hoover High School A (Hoover, AL) – Fully Paid
-    // Hunter College High School A (New York, NY) – Fully Paid
-    // Innovation Academy A (Alpharetta, GA) – Fully Paid
-    Irvington High School A (Fremont, CA) – Fully Paid
-    John Adams High School A (South Bend, IN) – Fully Paid
-    Johns Creek A (Johns Creek, GA) – Fully Paid
-    Kinkaid (Houston, TX) – Fully Paid
-    Lincoln-Way East High School (Frankfort, IL) – Fully Paid
-    Maggie Walker A (Richmond, VA) – Fully Paid
-    Mercer County Academic Team (Princeton, NJ 08540) – Fully Paid
-    Mexico High School A (Mexico, MO) – Fully Paid
-    Midtown High School (Atlanta, GA) – Fully Paid
-    Mira Loma High School (Sacramento, CA) – Fully Paid
-    Moberly High School (Moberly, MO) – Fully Paid
-    New Brighton Academic Team (New Brighton, MN) – Fully Paid
-    Norman High School (Norman, OK) – Fully Paid
-    Norris A (Firth, NE) – Fully Paid
-    Northmont High School A (Clayton, OH) – Fully Paid
-    Parkway West High School (Ballwin, MO) – Fully Paid
-    Paul Laurence Dunbar (Lexington, KY) – Fully Paid
-    Plymouth Academic Team (Plymouth, MN) – Fully Paid
-    Richard Montgomery High School A (Rockville, MD 20852) – Fully Paid
-    Saint Joseph (South Bend, IN) – Fully Paid
-    Solon High School (Solon, OH) – Fully Paid
-    St. Mark’s School of Texas (Dallas, TX) – Fully Paid
-    Stevenson A (Lincolnshire, IL) – Fully Paid
-    Thomas Jefferson Science & Tech A (Alexandria, VA 22312) – Fully Paid
-    Troy A (Troy, MI) – Fully Paid
-    Uni Lab A (Urbana, IL) – Fully Paid
-    University School of Nashville A (Nashville, TN) – Fully Paid
-    Walter Payton College Prep A (Chicago, IL) – Fully Paid
-    Washington High School A (Washington, MO) – Fully Paid
-    Waukee High School (Waukee, IA) – Fully Paid
-    West Point High School (Cullman, AL 35057) – Fully Paid
-    Winnebago High School (Winnebago, IL) – Fully Paid
-    Winston Churchill A (Potomac, MD) – Fully Paid
-    Woodland Regional High School (Beacon Falls, CT) – Fully Paid
-*/
